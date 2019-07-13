f776d739a15c64159ce2a13fa382e05130d1f1b4
Sajad Torkamani <sajadtorkamani1gmail.com>
2019-07-13 17:38:58 +0100

    Build

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 function log {
   echo "============================================="
   echo $1
@@ -11,16 +12,13 @@ $email="sajadtorkamani1@gmail.com"
 
 log "Generating SSH key"
 if [ -e $HOME/.ssh/id_rsa ]; then
-  log "SSH keys already generated. Skipping"
+  log "SSH keys already generated. Skipping."
 else
   ssh-keygen -t rsa -b 4096 -C email
 fi
 
-log "Installing ZSH"
-sudo apt-get -y install zsh
-
-log "Setting ZSH as default shell"
-chsh -s $(which zsh)
+log "Installing git"
+sudo apt-get -y install git
 
 log "Installing vim"
 sudo apt-get -y install vim
@@ -30,6 +28,19 @@ sudo apt-get -y install curl
 
 log "Installing checkinstall"
 sudo apt-get -y install checkinstall
+
+log "Installing xcape"
+sudo apt-get -y install gcc make pkg-config libx11-dev libxtst-dev libxi-dev
+
+log "Installing ZSH"
+if [[ $SHELL =~ "zsh" ]]; then
+  log "ZSH already set up. Skipping."
+else
+  sudo apt-get -y install zsh
+  log "Setting ZSH as default shell"
+  chsh -s $(which zsh)
+fi
+
 
 log "ALL DONE!"
 
