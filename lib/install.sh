117f5345a56373baefb7067d5bfd86a3df5fa5cf
Sajad Torkamani <sajadtorkamani1gmail.com>
2019-07-15 02:57:02 +0100

    More automation

--- lib/install.sh
+++ lib/install.sh
@@ -127,6 +127,30 @@ else
   sudo ln -s $DOTFILES_DIR/vim $HOME/.vim
 fi
 
+
+VSCODE_CONFIG_DIR="$HOME/.config/Code/User"
+if [[ -e $VSCODE_CONFIG_DIR/keybindings.json ]]; then
+  skip "$VSCODE_CONFIG_DIR/keybindings.json"
+else
+  log "Setting up VSCode keybindings"
+  sudo ln -s $DOTFILES_DIR/vscode/keybindings.json $VSCODE_CONFIG_DIR/keybindings.json
+fi
+
+if [[ -e $VSCODE_CONFIG_DIR/settings.json ]]; then
+  skip "$VSCODE_CONFIG_DIR/settings.json"
+else
+  log "Setting up VSCode settings.json"
+  sudo ln -s $DOTFILES_DIR/vscode/settings.json $VSCODE_CONFIG_DIR/settings.json
+fi
+
+if [[ -d $VSCODE_CONFIG_DIR/snippets ]]; then
+  skip "$VSCODE_CONFIG_DIR/snippets"
+else
+  log "Setting up VSCode snippets"
+  sudo ln -s $DOTFILES_DIR/vscode/snippets $VSCODE_CONFIG_DIR/snippets
+fi
+
+
 if [[ -e $HOME/.nvm ]]; then
   skip "nvm"
 else
---
+++ lib/vscode.sh
@@ -0,0 +1,2 @@
+code --install-extension vscodevim.vim
+
