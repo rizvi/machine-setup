4e03ab13f5a7f9bcc5e52b0ba84e42406d2b0944
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-07-20 15:00:25 +0100

    Update install-osx.sh

--- lib/install-osx.sh
+++ lib/install-osx.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
 
+# ---------------------------------------------------
+# Utility functions
+# ---------------------------------------------------
+# Pretty print a message.
+function log {
+  echo -e "\n"
+  echo "============================================="
+  echo $1
+  echo "============================================="
+}
+
+# Check if a cmd exists.
+function cmd_exists {
+  command -v $1 > /dev/null 2>&1
+}
+
+# Log a skiping message.
+function skip {
+ log "$1 already exists. Skipping"
+}
+
+
+# ---------------------------------------------------
+# Variables
+# ---------------------------------------------------
+EMAIL="sajadtorkamani1@gmail.com"
+DOTFILES_DIR=$HOME/.config/dotfiles
+
+
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+if [ -e $HOME/.ssh/id_rsa ]; then
+  skip "SSH keys"
+else
+  log "Generating SSH key"
+  ssh-keygen -t rsa -b 4096 -C $EMAIL
+fi
