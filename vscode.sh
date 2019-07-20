6b77509c0dd3efa6e6fb81ef66ca9a7fa8bf2c88
Sajad Torkamani <sajadtorkamani1@gmail.co>
2019-07-20 18:46:57 +0100

    Continue work

--- vscode.sh
+++ vscode.sh
@@ -1,6 +1,66 @@
 #!/bin/bash
 
 # ---------------------------------------------------
+# Utility functions
+# ---------------------------------------------------
+# Pretty print a message.
+function log {
+  echo -e "\n"
+  echo "============================================="
+  echo $1
+  echo "============================================="
+}
+
+# Check if a cmd exists.
+function cmd_exists {
+  command -v $1 > /dev/null 2>&1
+}
+
+# Log a skiping message.
+function skip {
+ log "$1 already exists. Skipping"
+}
+# ---------------------------------------------------
+
+# ---------------------------------------------------
+# Variables
+# ---------------------------------------------------
+DOTFILES_DIR="$HOME/.config/dotfiles"
+if [[ `uname` == 'Linux' ]]; then
+  CONFIG_DIR="$HOME/.config/Code/User"
+else
+  CONFIG_DIR="$HOME/Library/Application Support/Code/User"
+fi
+
+log "$CONFIG_DIR"
+
+# ---------------------------------------------------
+# Copy settings
+# ---------------------------------------------------
+if [[ -e "$CONFIG_DIR/keybindings.json" ]]; then
+  skip "$CONFIG_DIR/keybindings.json"
+else
+  log "Setting up VSCode keybindings"
+  ln -s "$DOTFILES_DIR/vscode/keybindings.json" "$CONFIG_DIR/keybindings.json"
+fi
+
+if [[ -e "$CONFIG_DIR/settings.json" ]]; then
+  skip "$CONFIG_DIR/settings.json"
+else
+  log "Setting up VSCode settings.json"
+  ln -s "$DOTFILES_DIR/vscode/settings.json" "$CONFIG_DIR/settings.json"
+fi
+
+if [[ -d "$CONFIG_DIR/snippets" ]]; then
+  skip "$CONFIG_DIR/snippets"
+else
+  log "Setting up VSCode snippets"
+  ln -s "$DOTFILES_DIR/vscode/snippets" "$CONFIG_DIR/snippets"
+fi
+# ---------------------------------------------------
+
+
+# ---------------------------------------------------
 # Install extensions
 # ---------------------------------------------------
 code --install-extension vscodevim.vim 
@@ -9,29 +69,4 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension equinusocio.vsc-material-theme
 code --install-extension pkief.material-icon-theme 
 code --install-extension esbenp.prettier-vscode
-# ---------------------------------------------------
-
-# Copy settings
-# ---------------------------------------------------
-VSCODE_CONFIG_DIR="$HOME/.config/Code/User"
-if [[ -e $VSCODE_CONFIG_DIR/keybindings.json ]]; then
-  skip "$VSCODE_CONFIG_DIR/keybindings.json"
-else
-  log "Setting up VSCode keybindings"
-  sudo ln -s $DOTFILES_DIR/vscode/keybindings.json $VSCODE_CONFIG_DIR/keybindings.json
-fi
-
-if [[ -e $VSCODE_CONFIG_DIR/settings.json ]]; then
-  skip "$VSCODE_CONFIG_DIR/settings.json"
-else
-  log "Setting up VSCode settings.json"
-  sudo ln -s $DOTFILES_DIR/vscode/settings.json $VSCODE_CONFIG_DIR/settings.json
-fi
-
-if [[ -d $VSCODE_CONFIG_DIR/snippets ]]; then
-  skip "$VSCODE_CONFIG_DIR/snippets"
-else
-  log "Setting up VSCode snippets"
-  sudo ln -s $DOTFILES_DIR/vscode/snippets $VSCODE_CONFIG_DIR/snippets
-fi
 # ---------------------------------------------------
