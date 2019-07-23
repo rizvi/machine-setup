aa07d8b6b5102dd5ecd32e2d0b04934b957b634b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2019-07-23 09:44:34 +0100

    Add clipy cask

--- macOS/casks.sh
+++ macOS/casks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 brew cask install alfred
+brew cask install clipy
 brew cask install google-chrome
 brew cask install karabiner-elements
 brew cask install mysqlworkbench
+brew cask install phpstorm
+brew cask install postman
 brew cask install sequel-pro
+brew cask install sourcetree
 brew cask install spectacle
 brew cask install visual-studio-code
-brew cask install phpstorm
 brew cask install webstorm
-brew cask install postman
-brew cask install sourcetree
